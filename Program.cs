d1f2fe06ea7d8b7e9b621c9a6d0ef48a91c2ae80
Azali4ik <110252875+Azali4ik@users.noreply.github.com>
2022-08-07 22:18:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + num);
+int CH = 1;
+while (CH <= num)
+ {
+  if (CH % 2 != 1)
+ {
+   Console.Write(CH + ", ");
+ }
+ else;
+CH++;
+}
+
+
